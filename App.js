1fce591f0cf7030a3b49fcbd773e82eb09227c43
Manvi ojha <ojhamanvi@gmail.com>
2019-10-20 18:04:35 +0530

    scroller implemented

--- App.js
+++ App.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] =  useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = ()  => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+ 
   return (
-    <View style = {{padding:30, borderColor:'blue', flexDirection:'row', height:300}}>
-      <View style = {{
-          backgroundColor:'red',
-          flex:1,
-        }}>
-         <Text>1</Text> 
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal"
+          style={styles.input} onChangeText = {goalInputHandler} value = {enteredGoal} />
+        <Button title="ADD" style={{ width: '19%', height: '100' }} onPress = {addGoalHandler} />
       </View>
-      <View style = {{
-         backgroundColor:'green',
-        flex:1,
-      }}>
-        <Text>2</Text>
-      </View>
-      <View style = {{
-         backgroundColor:'yellow',
-         flex:2,
-      }}>
-        <Text>3</Text>
-      </View>
-        
-      <View />
+      <ScrollView>{courseGoals.map((goal) => <View key = {goal} style = {styles.listItem}><Text>{goal}</Text></View>) }</ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
+    borderColor: 'blue'
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 10,
+  },
+  listItem : {
+    backgroundColor : '#cccfff',
+    padding:10,
+    marginVertical:10,
+    borderColor:'#000',
+    borderWidth:1,
+  }
 });
